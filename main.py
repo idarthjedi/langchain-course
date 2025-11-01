50db605fed1d8f37c6c74313968d5acb845cc508
Jediah Logiodice <jediah@logiodice.com>
2025-10-31 20:34:08 -0400

    ReAct search agent with structured output

--- main.py
+++ main.py
@@ -5,31 +5,67 @@ load_dotenv()
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
+
+USE_PYDANDIC_OUTPUT: bool = False
+
+#from openai import OpenAI
+#client = OpenAI()
+
+# client.models.list()
+
 tools = [TavilySearch()]
-llm = ChatOllama(model="deepseek-r1:70b")
+#llm = ChatOpenAI(model="gpt-4")
+llm = ChatOllama(model="llama3.3:70b")
 react_prompt = hub.pull("hwchase17/react")
+if USE_PYDANDIC_OUTPUT:
+    output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+    react_prompt_with_format_instructions = PromptTemplate(
+        template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+        input_variables=["input", "agent_scratchpad", "tool_names"],
+    ).partial(format_instructions=output_parser.get_format_instructions())
 
-agent = create_react_agent(
-    llm=llm,
-    tools=tools,
-    prompt=react_prompt,
-)
-
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+    agent = create_react_agent(
+        llm=llm,
+        tools=tools,
+        prompt=react_prompt_with_format_instructions,
+    )
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    extract_output = RunnableLambda(lambda x: x["output"])
+    parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+    chain = agent_executor | extract_output | parse_output
+else:
+    structured_llm = llm.with_structured_output(AgentResponse)
+    react_prompt_with_format_instructions = PromptTemplate(
+        template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+        input_variables=["input", "agent_scratchpad", "tool_names"],
+    ).partial(format_instructions="")
+    agent = create_react_agent(
+        llm=llm,
+        tools=tools,
+        prompt=react_prompt_with_format_instructions,
+    )
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    extract_output = RunnableLambda(lambda x: x["output"])
+    chain = agent_executor | extract_output | structured_llm
 
 
 def main():
     result = chain.invoke(
         input={
-            "input": "search on linkedin for 3 remote eligible job postings for a distinguished architect using langchain, then list their details",
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
         }
     )
     print(result)
 
+
 if __name__ == "__main__":
     main()
-
