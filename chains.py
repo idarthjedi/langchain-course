44b5c7e68f2e24cf94425dbff469d72b365157d6
Jediah Logiodice <jediah@logiodice.com>
2025-11-12 20:16:27 -0500

    Creation of a Reflexion agent workflow

--- chains.py
+++ chains.py
@@ -8,8 +8,15 @@ from langchain_core.output_parsers.openai_tools import (
 )
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
+
+from schemas import AnswerQuestion, ReviseAnswer
+
+llm = ChatOllama(model="gpt-oss:120b")
+parser = JsonOutputToolsParser(return_id=True)
+parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
 
 actor_prompt_template = ChatPromptTemplate.from_messages(
     [
@@ -28,3 +35,40 @@ actor_prompt_template = ChatPromptTemplate.from_messages(
 ).partial(
     time=lambda: datetime.datetime.now().isoformat(),
 )
+
+first_responder_prompt_template = actor_prompt_template.partial(
+    first_instruction="Provide a detailed ~250 word answer."
+)
+
+first_responder = first_responder_prompt_template | llm.bind_tools(
+    tools=[AnswerQuestion], tool_choice="AnswerQuestion"
+)
+
+
+revise_instructions = """Revise your previous answer using the new information.
+    - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit). In form of:
+            - [1] https://example.com
+            - [2] https://example.com
+    - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
+
+revisor = actor_prompt_template.partial(
+    first_instruction=revise_instructions,
+) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
+
+if __name__ == "__main__":
+    human_message = HumanMessage(
+        content="Write about AI-Powered SOC & the autonomous SOC problem domain, "
+        "list startups that do it and have raised capital"
+    )
+
+    chain = (
+        first_responder_prompt_template
+        | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
+        | parser_pydantic
+    )
+
+    res = chain.invoke(input={"messages": [human_message]})
+    print(res)
--- main.py
+++ main.py
@@ -1,8 +1,51 @@
-import os
+from typing import List
 
 from dotenv import load_dotenv
+from langchain_core.messages import BaseMessage, HumanMessage, ToolMessage
+from langgraph.graph import END, MessageGraph, StateGraph
+
+from chains import first_responder, revisor
+from tool_executor import execute_tools
 
 load_dotenv()
 
+MAX_ITERATIONS = 2
+builder = MessageGraph()
+builder.add_node("draft", first_responder)
+builder.add_node("execute_tools", execute_tools)
+builder.add_node("revise", revisor)
+
+builder.add_edge("draft", "execute_tools")
+builder.add_edge("execute_tools", "revise")
+
+
+def event_loop(state: List[BaseMessage]) -> str:
+    count_tool_visits = sum(isinstance(item, ToolMessage) for item in state)
+    num_iterations = count_tool_visits
+    if num_iterations > MAX_ITERATIONS:
+        return END
+    return "execute_tools"
+
+
+builder.add_conditional_edges(
+    "revise", event_loop, path_map={END: END, "execute_tools": "execute_tools"}
+)
+
+builder.set_entry_point("draft")
+graph = builder.compile()
+graph.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
+
 if __name__ == "__main__":
-    print(os.environ.get("ENV"))
+    print("Reflexion Agent Starting...")
+    res = graph.invoke("Write about AI-Powered SOC & the autonomous SOC problem domain, list startups that do it and have raised capital")
+
+#        {
+#            "messages": [
+#                HumanMessage(
+#                    content="Write about AI-Powered SOC & the autonomous SOC problem domain, list startups that do it and have raised capital"
+#                )
+#            ]
+#        }
+#    )
+    print(res[-1].tool_calls[0]["args"]["answer"])
